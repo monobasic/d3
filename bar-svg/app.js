995b7656e99d706671506fe65d078a78db49c942
André Abt <info@andre-abt.com>
2020-01-09 13:11:11 +0100

    wip

--- bar-svg/app.js
+++ bar-svg/app.js
@@ -2,10 +2,10 @@ const data = [];
 const chartWidth = 800;
 const chartHeight = 400;
 const barPadding = 5;
-const barCount = Math.floor(d3.randomUniform(10, 30)());
+const barCount = Math.floor(d3.randomUniform(4, 30)());
 
 for (var i = 0; i < barCount; i++) {
-  data.push(Math.floor(d3.randomUniform(1, 100)()));
+  data.push(Math.floor(d3.randomUniform(24, 390)()));
 }
 
 // Create SVG
@@ -31,5 +31,27 @@ svg
   .attr('height', function (d) {
     return d + 'px';
   })
-  .attr('fill', '#7ED26D')
+  .attr('fill', '#7ED26D');
+
+// Create labels
+svg
+  .selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .text(function (d) {
+    return d;
+  })
+  .attr('x', function (d, i) {
+    return i * (chartWidth / data.length) +
+      (chartWidth / data.length - barPadding) / 2
+  })
+  .attr('y', function (d) {
+    return chartHeight - d + 15;
+  })
+  .attr('font-size', '15px')
+  .attr('fill', '#ffffff')
+  .attr('text-anchor', 'middle')
+
+
 
